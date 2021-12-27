74b36e898757a427db98ffb2c3fca62e6381e1a0
Nam Dao <bk.namapple@gmail.com>
2021-12-27 23:29:33 +0700

    TextField learning

--- flutter_text_widget/lib/main.dart
+++ flutter_text_widget/lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_text_widget/screens/home/home_screen.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
--- flutter_text_widget/lib/screens/home/home_screen.dart
+++ flutter_text_widget/lib/screens/home/home_screen.dart
@@ -8,102 +8,123 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  var _textColor = Colors.red;
+  Color _textColor = Colors.red;
+  String? namePerson;
+  final _nameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My App'),
+        title: const Text('Welcome'),
       ),
-      body: Center(
-        child: Container(
-          height: 400,
-          width: 400,
-          decoration: BoxDecoration(
-            color: Colors.blueGrey[100],
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(
-              color: Colors.red,
-              width: 5,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 15,
+              vertical: 10,
+            ),
+            child: TextField(
+              onChanged: (value) {
+                setState(() {
+                  namePerson = value;
+                });
+              },
+              controller: _nameController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter your name',
+              ),
             ),
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              _TextComponent(
-                greetingText: 'Xin chao ',
-                fontSize: 40,
-                textColor: _textColor,
+          const SizedBox(
+            height: 10.0,
+          ),
+          Container(
+            height: 200,
+            width: 600,
+            decoration: BoxDecoration(
+              color: Colors.blueGrey[50],
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                color: Colors.red,
+                width: 5,
               ),
-              SizedBox(
-                height: 100,
-                width: 100,
-                child: InkWell(
-                    child: Icon(
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _TextComponent(
+                  namePerson: namePerson ?? 'User!',
+                  greetingText: 'Xin chao ',
+                  fontSize: 40,
+                  textColor: _textColor,
+                ),
+                SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: InkWell(
+                    child: const Icon(
                       Icons.access_alarm,
                       color: Colors.red,
                     ),
                     onTap: () {
                       setState(() {
                         _textColor = Colors.green;
+                        namePerson = _nameController.text;
                       });
-                    }),
-              ),
-            ],
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
 }
 
-class _TextComponent extends StatefulWidget {
+class _TextComponent extends StatelessWidget {
   const _TextComponent({
     Key? key,
     required this.greetingText,
     this.fontSize = 50,
     this.textColor = Colors.green,
+    this.namePerson = 'User!',
   }) : super(key: key);
   final String greetingText;
   final double fontSize;
-  final textColor;
-  @override
-  State<_TextComponent> createState() => _TextComponentState();
-}
-
-class _TextComponentState extends State<_TextComponent> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-  }
+  final Color textColor;
+  final String namePerson;
 
   @override
   Widget build(BuildContext context) {
     return RichText(
       text: TextSpan(
-        text: widget.greetingText,
+        text: greetingText,
         style: TextStyle(
-          fontSize: widget.fontSize,
-          color: widget.textColor,
+          fontSize: fontSize,
+          color: textColor,
         ),
-        children: const <TextSpan>[
+        children: <TextSpan>[
           TextSpan(
-            text: 'Beautiful ',
-            style: TextStyle(
-              color: Colors.green,
-            ),
-          ),
-          TextSpan(
-            text: 'World',
-            style: TextStyle(
-              color: Colors.blue,
+            text: namePerson,
+            style: const TextStyle(
+              color: Colors.black,
+              fontStyle: FontStyle.italic,
             ),
           ),
         ],
